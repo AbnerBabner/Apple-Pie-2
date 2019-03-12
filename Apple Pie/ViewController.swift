690563af84a4dc26c9612fb0638210e403d964d0
Abner Mardjuki <abnermardjuki@Abners-MacBook-Air.local>
2019-03-12 15:36:18 +0700

    UI with connections

---
+++ Apple Pie/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Apple Pie
+//
+//  Created by Sim Yong Seng on 25/2/19.
+//  Copyright © 2019 Sim Yong Seng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var treeImageView: UIImageView!
+    @IBOutlet weak var correctWordLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    @IBOutlet var letterButtons: [UIButton]!
+    
+    @IBAction func buttonTap(_ sender: UIButton) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
