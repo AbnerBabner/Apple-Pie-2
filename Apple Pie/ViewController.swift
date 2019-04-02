3d99b7863bb88ad1fa42c6d5e1c6867ed78e5338
AbnerBabner <abnermardjuki@yahoo.co.id>
2019-04-02 15:18:21 +0700

    Part 4 done

--- Apple Pie/ViewController.swift
+++ Apple Pie/ViewController.swift
@@ -10,16 +10,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var treeImageView: UIImageView!
-    @IBOutlet weak var correctWordLabel: UILabel!
+    @IBOutlet weak var treeImageView:
+    UIImageView!
+    @IBOutlet weak var correctWordLabel:
+    UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
     
     @IBOutlet var letterButtons: [UIButton]!
-    
     @IBAction func buttonTap(_ sender: UIButton) {
+        sender.isEnabled = false
+        let letterString = sender.title(for: .normal)!
+        let letter = Character(letterString.lowercased())
+        currentGame.playerGuessed(letter: letter)
+        print(currentGame.incorrectMovesRemaining)
+        print(currentGame.formattedWord)
     }
+    
+    var listOfWords =
+        ["buccanner","basketball","soccer"]
+    var incorrectMovesAllowed = 7
+    var totalWins = 0
+    var totalLosses = 0
+    var currentGame: Game!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        newRound()
+    }
+    
+    func newRound() {
+        let newWord = listOfWords.removeFirst()
+        currentGame = Game(word: newWord,
+                           incorrectMovesRemaining: incorrectMovesAllowed, guessedLetters: [])
+        updateUI()
+    
+    }
+
+    func updateUI() {
+        var letters = [String]()
+        for letter in currentGame.formattedWord {
+            letters.append(String(letter))
+        }
+        let wordWithSpacing = letters.joined(separator: " ")
+        correctWordLabel.text = wordWithSpacing
+        
+        scoreLabel.text = "Wins: \(totalWins), Losses: \(totalLosses)"
+        treeImageView.image = UIImage(named: "Tree \(currentGame.incorrectMovesRemaining)")
+        
     }
 }
-
